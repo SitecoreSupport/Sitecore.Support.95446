7a6c877780dce89a8fb0878f73f41a6289fc1111
DK\obr <obr@sitecore.net>
2017-06-13 11:53:36 +0300

    commit

---
+++ src/Sitecore.Support.95446/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.95446")]
+[assembly: AssemblyProduct("Sitecore.Support.95446")]
+[assembly: ComVisible(false)]
